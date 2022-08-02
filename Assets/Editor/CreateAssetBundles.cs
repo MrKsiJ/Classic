c62166d7fa4e77ee64c7d04d2cbdf9b70d1f4af9
Dmitry Batkovich <57679050+MrKsiJ@users.noreply.github.com>
2022-08-02 21:58:06 +0300

    Add files via upload

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles 
+{
+    [MenuItem("Assets/BuildAssetBundels")]
+   static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundels", BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+}
